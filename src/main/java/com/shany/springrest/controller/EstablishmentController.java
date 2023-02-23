03348930e25caa3d827ae888cce1ff5a0fb53ec8
ShanyGui <shany.guitou@gmail.com>
2023-02-23 12:50:41 +0100

    Version Finale

--- src/main/java/com/shany/springrest/controller/EstablishmentController.java
+++ src/main/java/com/shany/springrest/controller/EstablishmentController.java
@@ -9,8 +9,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,7 +29,6 @@ public class EstablishmentController {
 
 	@PostMapping("/add")
 	public ResponseEntity<Establishment> addOne(@RequestBody Establishment establishment) {
-
 		this.establishmentdao.save(establishment);
 		return new ResponseEntity<Establishment>(establishment, HttpStatus.CREATED);
 	}
@@ -46,11 +47,27 @@ public class EstablishmentController {
 	@GetMapping("/{id}")
 	public ResponseEntity<Establishment> findOne(@PathVariable Integer id) {
 		Optional<Establishment> optionEstablishment = this.establishmentdao.findById(id);
-
 		return optionEstablishment.isPresent()
 				? new ResponseEntity<Establishment>(optionEstablishment.get(), HttpStatus.OK)
 				: new ResponseEntity<Establishment>(HttpStatus.NOT_FOUND);
-
 	}
 
+	@PutMapping("/update/{id}")
+	public ResponseEntity<Establishment> update(@RequestBody Establishment establishment, @PathVariable Integer id) {
+		Optional<Establishment> updateEstablishment = establishmentdao.findById(id);
+		
+		if(updateEstablishment.isPresent()) {
+			updateEstablishment.get().setName(establishment.getName());
+			updateEstablishment.get().setAdress(establishment.getAdress());
+			updateEstablishment.get().setType(establishment.getType());
+			updateEstablishment.get().setPhoneNumber(establishment.getPhoneNumber());
+			//updateEstablishment.get().setLogo(establishment.getLogo());
+			this.establishmentdao.save(updateEstablishment.get());
+			return new ResponseEntity<Establishment>(establishment, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<Establishment>(establishment, HttpStatus.NOT_FOUND);
+		}
+				 
+	}
+	
 }
